1df7a664de6d25513a1c2c56569be19ebbfa94f7
lombardo-chcg <dpa.lombardo@gmail.com>
2017-03-16 22:45:26 -0500

    add json support

--- src/main/scala/com/lombardo/app/DemoApiServlet.scala
+++ src/main/scala/com/lombardo/app/DemoApiServlet.scala
@@ -1,16 +1,19 @@
 package com.lombardo.app
 
 import org.scalatra._
+import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.json._
 
-class DemoApiServlet extends DemoapiStack {
+class DemoApiServlet extends DemoapiStack with JacksonJsonSupport {
+
+  protected implicit val jsonFormats: Formats = DefaultFormats
+
+  before() {
+    contentType = formats("json")
+  }
 
   get("/") {
-    <html>
-      <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
-      </body>
-    </html>
+    ("message" -> "hello world")
   }
 
 }
