5be654792bf6a1101a3eee84226da5bcf48d86b2
lombardo-chcg <dpa.lombardo@gmail.com>
2017-05-11 06:46:22 -0500

    update scripts and readme

--- README.md
+++ README.md
@@ -1,26 +1,25 @@
-# demo-api #
+# Scrabble Helper API #
 
-## Build & Run ##
+## RUN Intellij Mode ##
+
+Import `JettyLauncher.xml` as an IntelliJ Run Configuration
+
+Access via `http://localhost:8080/words/docker` 
+
+## RUN Docker Mode ##
+```
+docker-compose up
+```
+
+Access via `http://<YOUR_DOCKERHOST>:18080/words/docker`
+
+## RUN SBT Mode ##
 
 ```sh
-$ cd demo_api
-$ ./sbt
+$ source /scripts/launch-postgres.sh
+$ sbt
 > jetty:start
-> browse
 ```
 
-If `browse` doesn't launch your browser, manually open [http://localhost:8080/](http://localhost:8080/) in your browser.
+Access via `http://localhost:8080/words/docker` 
 
-install `psql` client inside a docker container?
-
-```
-apt-get update
-apt-get install postgresql-client
-psql -h postgres -p 5432 -U postgres
-```
-
-
-```
-export POSTGRES_HOST=dockerhost
-export POSTGRES_PORT=5431
-```
--- scripts/launch-postgres.sh
+++ scripts/launch-postgres.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-db_container="postgres"
+
+if [[ $(docker-machine active 2>/dev/null) ]]; then
+  DOCKER_IP=$(docker-machine ip $(docker-machine active))
+else
+  DOCKER_IP="localhost"
+fi
+
+export POSTGRES_HOST=$DOCKER_IP
+export POSTGRES_PORT=5431
+export POSTGRES_USER=postgres
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_DB=scrabble_helper
+
+echo $DOCKER_IP
 
 docker-compose down
 docker-compose up -d postgres
-
-docker cp db/dict.csv $db_container:/dict.csv
-
-docker cp db/create_greetings_table.sql $db_container:/docker-entrypoint-initdb.d/create_greetings_table.sql
-docker cp db/seed_greetings_table.sql $db_container:/docker-entrypoint-initdb.d/seed_greetings_table.sql
-docker cp db/create_words_table.sql $db_container:/docker-entrypoint-initdb.d/create_words_table.sql
-docker cp db/seed_words_table.sql $db_container:/docker-entrypoint-initdb.d/seed_words_table.sql
