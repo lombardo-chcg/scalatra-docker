1529b91342e677616b7475c863fb3ee783fc1965
lombardo-chcg <dpa.lombardo@gmail.com>
2017-05-17 21:14:26 -0500

    add timer and update docker compose

--- src/main/scala/com/lombardo/app/services/RedisService.scala
+++ src/main/scala/com/lombardo/app/services/RedisService.scala
@@ -7,24 +7,28 @@ import org.slf4j.LoggerFactory
 
 class RedisService {
 
+  val redisHost = sys.env("REDIS_HOST")
+  val redisPort = sys.env("REDIS_PORT").toInt
+
   val logger =  LoggerFactory.getLogger(getClass)
-  val redis = new RedisClient("localhost", 32788)
+  val redis = new RedisClient(redisHost, redisPort)
 
   def findAllWithSearch(resource: String, column: String, searchTermList: List[String]) : Option[List[Map[String, String]]] = {
-    val output = searchTermList.map(word => {
+    logger.info(s"""Making ${searchTermList.length} Redis requests""")
+
+    val redisResultSet = searchTermList.map(word => {
       redis.lrange(word, 0, -1)
     })
 
-    val returnVal = output.flatten.flatten.map(x => {
-      x match {
+    val unwrappedResultSet = redisResultSet.flatten.flatten.map({
         case Some(thing) => thing
         case None => None
-      }
     })
 
     try {
-      val resultSet = returnVal.map(x => {
+      val resultSet = unwrappedResultSet.map(x => {
         val split = x.toString.split(",")
+
         Map("word" -> split(0), "points" -> split(1))
       })
 
--- src/main/scala/com/lombardo/app/services/WordService.scala
+++ src/main/scala/com/lombardo/app/services/WordService.scala
@@ -1,5 +1,7 @@
 package com.lombardo.app.services
 
+import java.util.concurrent.TimeUnit
+
 import org.slf4j.LoggerFactory
 import com.lombardo.app.services.Model._
 
@@ -13,14 +15,14 @@ class WordService {
   val columnName = "canonical_word"
 
   def findAll(input: String, prefix: String, suffix: String): SearchResult = {
+    val t0 = System.nanoTime
+
     val subSets = getWordSubsets(input)
 
     val rawResultSet = redisService.findAllWithSearch(tableName, columnName, subSets) match {
       case Some(results) => results.map(result => new Word(result("word"), result("points").toInt))
       case None => List()
     }
-
-    logger.info(s"""${rawResultSet.length} words found""")
 
     val suffixFilteredSet = suffix match {
       case "" => rawResultSet
@@ -31,6 +33,11 @@ class WordService {
       case "" => suffixFilteredSet
       case _ => suffixFilteredSet.filter(_.word.startsWith(prefix))
     }
+
+    val t1 = System.nanoTime
+    val duration = TimeUnit.MILLISECONDS.convert((t1 - t0), TimeUnit.NANOSECONDS);
+
+    logger.info(s"""${rawResultSet.length} raw results || ${prefixFilteredSet.length} returned after filters applied || elapsed time ${duration} ms""")
 
     new SearchResult(prefixFilteredSet.length, prefixFilteredSet.sortWith(_.points > _.points))
   }
